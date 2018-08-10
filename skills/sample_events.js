e713a5c5e6447db1374fb3c1518ea1ba08220e50
Jennifer Tran <jennifer.tran101@gmail.com>
2018-08-10 15:54:44 -0500

    Added .env file for deployment

--- skills/sample_events.js
+++ skills/sample_events.js
@@ -1,9 +1,15 @@
-module.exports = function(controller) {
+module.exports = function (controller) {
 
-    controller.on('user_channel_join,user_group_join', function(bot, message) {
+  controller.on('user_channel_join,user_group_join', function (bot, message) {
 
-        bot.reply(message, 'Welcome, <@' + message.user + '>');
+    bot.reply(message, 'Welcome, <@' + message.user + '>');
 
-    });
+  });
 
-}
+  controller.on('team_join', function (bot, message) {
+
+    bot.reply(message, 'Welcome to devClub, <@' + message.user + '>');
+
+  });
+
+};
